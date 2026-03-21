960c62a3bf021d2d962962e32bf98c4ff77c7994
femnixx <femnitality@gmail.com>
2026-03-21 19:17:23 +0700

    made it so that owner recognition is more strict

--- app.py
+++ app.py
@@ -22,15 +22,18 @@ while True:
       face_locations = face_recognition.face_locations(rgb_small_frame)
       face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
+      current_results = []
+
       for (top, right, bottom, left), encoding in zip(face_locations, face_encodings):
          status_text, color = brain.analyze_face(frame, (top, right, bottom, left), encoding)
+         current_results.append((status_text, color))
 
-   for (top, right, bottom, left) in face_locations:
-      t, r, b, l = top * 4, right * 4, bottom * 4, left * 4
-      
-      cv2.rectangle(frame, (l, t), (r, b), color, 2)
-      cv2.putText(frame, status_text, (l, t - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
- 
+   if 'current_results' in locals():
+      for (top, right, bottom, left), (st, col) in zip(face_locations, current_results):
+         t, r, b, l = top * 4, right * 4, bottom * 4, left * 4 
+         cv2.rectangle(frame, (l, t), (r, b), color, 2)
+         cv2.putText(frame, status_text, (l, t - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+   
    cv2.imshow('AI Lock System', frame)
 
    if cv2.waitKey(1) & 0xFF == ord('q'):
