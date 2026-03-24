cc5e911747d5362c81777c59dfb4cdad1805c02e
femnixx <femnitality@gmail.com>
2026-03-24 11:49:57 +0700

    chore: added gitignore and removed venv from tracking

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 from deepface import DeepFace
+import os
 
 cap = cv2.VideoCapture(0)
 
@@ -9,6 +10,22 @@ if not cap.isOpened():
 
 print("Webcam opened. Press 'q' to quit")
 
+AUTHORIZED_DIR = "authorized_faces"
+SUSPICIOUS_EMOTIONS= ['angry', 'fear', 'disgust']
+
+def is_authorized(frame):
+    try: 
+        results = DeepFace.find(
+            img_path=frame
+            db_path=AUTHORIZED_DIR
+            enforce_detection=False
+            silent=True
+        )
+        if len(results) > 0 and len(results[0]) >0:
+            return True
+        return False
+    except Exception:
+        return False
 
 
 while True: 
@@ -22,10 +39,27 @@ while True:
         result = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False)
 
         emotion = result[0]['dominant_emotion']
+        region = result[0]['region']
 
-        cv2.putText(frame, f"Emotion: {emotion}", (50, 50),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2
-                    )
+        x, y, w, h = region['x'], region['y'], region['w'], region['h']
+
+        authorized = is_authorized(frame)
+
+        if authorized: 
+            if emotion in SUSPICIOUS_EMOTIONS:
+                color = (0, 165, 255)
+                label = f"AUTHORIZED - SUSPICIOUS: {emotion}"
+            else: 
+                color = (0, 255, 0)
+                label = f"AUTHORIZED: {emotion}"
+        else: 
+            color = (0, 0, 255)
+            label = "INTRUDER DETECTED"
+        
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+        cv2.putText(frame, label, (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+        
     except Exception as e: 
         print(f"Detection error: {e}")
 
